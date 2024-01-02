7c785898894de6a3be27903b84db607f7b31d4be
Ulysse Gérard <thevoodoos@gmail.com>
2024-01-02 21:07:09 +0100

    std: improve encoding performances

--- lib/std/std.ml
+++ lib/std/std.ml
@@ -10,16 +10,11 @@ module Yojson = struct
 end
 
 module Encodings = struct
-  let marshal_to_jstr t =
-    Marshal.to_string t [] |> Base64.encode_string |> Jstr.v
-
+  let marshal_to_jstr t = Marshal.to_string t [] |> Jstr.binary_of_octets
   let marshal_to_jv t = marshal_to_jstr t |> Jv.of_jstr
 
   let unmarshal_jstr jstr =
-    let open Result.Infix in
-    try
-      let+ str = Jstr.to_string jstr |> Base64.decode in
-      Marshal.from_string str 0
+    try Ok (Marshal.from_string (Jstr.binary_to_octets jstr) 0)
     with _ -> Error (`Msg "Failed to unmarshal data")
 
   let unmarshal_jv jv = Jv.to_jstr jv |> unmarshal_jstr
