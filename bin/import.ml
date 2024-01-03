31656ab862d12c7c62094c8feee16f5908bafc6c
Ulysse Gérard <thevoodoos@gmail.com>
2024-01-03 22:01:12 +0100

    utils: add optional name when timing things

--- bin/import.ml
+++ bin/import.ml
@@ -1,9 +1,10 @@
 include Std
 
 module Utils = struct
-  let with_timing f =
+  let with_timing ?(name = "") f =
     let t = Brr.Performance.now_ms Brr.G.performance in
     let res = f () in
-    Brr.Console.log [ "Took"; Brr.Performance.now_ms Brr.G.performance -. t ];
+    Brr.Console.log
+      [ name; "took"; Brr.Performance.now_ms Brr.G.performance -. t ];
     res
 end
