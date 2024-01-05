1a29fbb0375fe6b783185b5e9e16ef6a4f427476
Ulysse Gérard <thevoodoos@gmail.com>
2024-01-05 20:37:43 +0100

    lwd_ui: start moving table logic to the lib

---
+++ lib/brr_lwd_ui/table.ml
@@ -0,0 +1,26 @@
+open Std
+open Brrer
+open Brr
+open Brr_lwd
+
+module Columns = struct
+  type column = { name : string; css_size : string; content : Elwd.t Elwd.col }
+  type t = column array
+
+  let v name css_size content = { name; css_size; content }
+
+  let to_header t =
+    let cells =
+      Array.fold_right t ~init:[] ~f:(fun { content; _ } acc ->
+          `R (Elwd.div content) :: acc)
+    in
+    Elwd.div cells
+
+  let set_style t elt =
+    let template =
+      Array.to_string ~sep:" " (fun { css_size; _ } -> css_size) t
+    in
+    let value = Jstr.v template in
+    El.set_inline_style El.Style.grid_template_columns value elt;
+    elt
+end
