9b57eee7f16745c69210d6901c0f348ac425e7c0
Ulysse Gérard <thevoodoos@gmail.com>
2024-01-13 21:09:54 +0100

    worker_api: listeners

--- lib/worker_api/worker_api.ml
+++ lib/worker_api/worker_api.ml
@@ -1,45 +1,66 @@
 open Std
 open Brr
 
+type tag = Block of int | Int of int
+
+let tag_of v =
+  (* wow wow wow. is that okayyish ?*)
+  let obj = Obj.repr v in
+  if Obj.is_block obj then Block (Obj.tag obj) else Int (Obj.magic obj)
+
 module type Queries = sig
   type 'a query
+  type 'a event
 end
-
-type 'a with_uuid = { uuid : string; message : 'a }
 
 module Make (Q : Queries) = struct
   type error = [ `Jv of Jv.Error.t | `Msg of string ]
   type 'a query = 'a Q.query
-  (* todo:check that the worker and the client share the same api? *)
+  type 'a event = 'a Q.event
+  type 'a with_uuid = { uuid : string; data : 'a }
+  type 'a message = Answer of 'a with_uuid | Event of ('a event * 'a)
+  type listener = string
 
-  let futures : (string, Jv.t -> unit) Hashtbl.t = Hashtbl.create 64
+  (* todo:check that the worker and the client share the same api? *)
 
   module Start_client (P : sig
     val url : string
   end) =
   struct
+    let futures : (string, Jv.t -> unit) Hashtbl.t = Hashtbl.create 64
+    let listeners : (tag, Jv.t -> unit) Hashtbl.t = Hashtbl.create 64
     let worker = Brr_webworkers.Worker.create @@ Jstr.of_string P.url
 
     let query (type a) (query : a query) : (a, error) Fut.result =
       let uuid = new_uuid_v4 () |> Uuidm.to_string in
       let fut, set = Fut.create () in
       let set jv = set @@ Encodings.unmarshal_jv jv in
-      let query = { uuid; message = query } |> Encodings.marshal_to_jstr in
+      let query = { uuid; data = query } |> Encodings.marshal_to_jstr in
       Brr_webworkers.Worker.post worker (Jv.of_jstr query);
       Hashtbl.add futures uuid set;
       fut
+
+    let listen (type a) (event : a event) ~(f : a -> unit) : listener =
+      let uuid = new_uuid_v4 () |> Uuidm.to_string in
+      let set jv = f (Obj.magic jv) in
+      Hashtbl.add listeners (tag_of event) set;
+      uuid
 
     let on_message ev =
       let message = Ev.as_type ev in
       let open Result in
       ignore (* TODO: handler errors *)
       @@
-      let+ ({ uuid; message } : Jv.t with_uuid) =
+      let+ message =
         Brr_io.Message.Ev.data message |> Jv.to_jstr |> Encodings.unmarshal_jstr
       in
-      let set = Hashtbl.find futures uuid in
-      Hashtbl.remove futures uuid;
-      set message
+      match message with
+      | Event (e, v) ->
+          Hashtbl.find_all listeners (tag_of e) |> List.iter ~f:(fun f -> f v)
+      | Answer { uuid; data } ->
+          let f = Hashtbl.find futures uuid in
+          Hashtbl.remove futures uuid;
+          f data
 
     let _ =
       Ev.listen Brr_io.Message.Ev.message on_message
@@ -49,6 +70,11 @@ module Make (Q : Queries) = struct
   module type Worker_impl = functor () -> sig
     val on_query : 'a query -> ('a, error) Fut.result
   end
+
+  let dispatch_event (type a) (e : a event) (v : a) =
+    (* let data = Encodings.marshal_to_jstr |> Jv.of_jstr in *)
+    Brr_webworkers.Worker.G.post
+      (Encodings.marshal_to_jstr (Event (e, v)) |> Jv.of_jstr)
 
   (** Execute W's body and configure messaging *)
   module Make_worker (W : Worker_impl) = struct
@@ -60,14 +86,14 @@ module Make (Q : Queries) = struct
       let open Result in
       ignore (* TODO: handler errors *)
       @@
-      let+ ({ uuid; message } : 'a query with_uuid) =
+      let+ ({ uuid; data } : 'a query with_uuid) =
         Brr_io.Message.Ev.data message |> Jv.to_jstr |> Encodings.unmarshal_jstr
       in
       let open Fut.Result_syntax in
-      let+ result = W.on_query message in
-      let message = Encodings.marshal_to_jstr result |> Jv.of_jstr in
+      let+ result = W.on_query data in
+      let data = Encodings.marshal_to_jstr result |> Jv.of_jstr in
       Brr_webworkers.Worker.G.post
-        (Encodings.marshal_to_jstr { uuid; message } |> Jv.of_jstr)
+        (Encodings.marshal_to_jstr (Answer { uuid; data }) |> Jv.of_jstr)
 
     let _ = Ev.listen Brr_io.Message.Ev.message on_message G.target
   end
--- lib/worker_api/worker_api.mli
+++ lib/worker_api/worker_api.mli
@@ -6,11 +6,16 @@ module type Queries = sig
            | Next_int : int -> int query
            | Previous_letters : char -> char list query ]}
   *)
+
+  type 'a event
+  (** Events that can be listened to *)
 end
 
 module Make : functor (Q : Queries) -> sig
   type error = [ `Jv of Jv.Error.t | `Msg of string ]
   type 'a query = 'a Q.query
+  type 'a event = 'a Q.event
+  type listener
 
   (** The [Client] functor should be use to open a connexion to the worker and
       provides a standardized interface for querying it. *)
@@ -21,13 +26,23 @@ module Make : functor (Q : Queries) -> sig
      end)
     -> sig
     val worker : Brr_webworkers.Worker.t
+
     val query : 'a query -> ('a, error) Fut.result
+    (** Send one query to the worker and return a future with the answer. *)
+
+    val listen : 'a event -> f:('a -> unit) -> listener
+    (** It returns a [listener] handle that can be used to cancel the 
+        subscription later-on (TODO). *)
   end
 
   module type Worker_impl = functor () -> sig
     val on_query : 'a query -> ('a, error) Fut.result
     (** [on_query q] should return the result of the query q *)
   end
+
+  val dispatch_event : 'a event -> 'a -> unit
+  (** [dispactch_event] should be used from inside the worker to send events
+      to the client. *)
 
   (** Use [Make_worker] to generate the body of the worker. The generative
       functor given in parameter contian the program t be executed, and should
