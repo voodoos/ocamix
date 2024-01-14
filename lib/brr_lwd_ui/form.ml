ab0a873713d8f09d72f1228e7a92a5da0ff9e3e1
Ulysse Gérard <thevoodoos@gmail.com>
2024-01-14 22:37:21 +0100

    form: complete rework wip
    Still too complex

--- lib/brr_lwd_ui/form.ml
+++ lib/brr_lwd_ui/form.ml
@@ -23,9 +23,10 @@ module Field = struct
   let make ?d ?at ?ev (type value p)
       (module Field : S with type parameters = p and type value = value) ~id
       (p : p) ?(validate = fun (v : value) -> Ok v) () =
-    let open Lwd_infix in
-    let$ elt = Field.render ?d ?at ?ev p () in
-    { id; elt; value = (fun () -> Field.get_value elt); validate }
+    let elt = Field.render ?d ?at ?ev p () in
+    Lwd.map elt ~f:(fun elt ->
+        let value () = Field.get_value elt in
+        { id; elt; value; validate })
 
   module Text_input = struct
     type value = string
@@ -51,25 +52,31 @@ module type Form = sig
   type t
 
   val default : t
-  val fields : t form_field list Lwd.t
+  val fields : t form_field Lwd.t Lwd_seq.t Lwd.t
 end
 
 let create ?d ?at ?ev (type t) (module Form : Form with type t = t) on_submit :
     Elwd.t Lwd.t =
-  let open Lwd_infix in
-  let$* fields = Form.fields in
-  let elts, value =
-    List.fold_left fields
-      ~init:([], fun () -> Form.default)
-      ~f:(fun (elts, form) (F (field, mapper)) ->
-        let value () = field.value () |> field.validate in
-        (field.elt :: elts, fun () -> mapper (form ()) @@ value ()))
+  let fields =
+    Lwd_seq.lift Form.fields
+    |> Lwd_seq.fold_monoid
+         (fun (F (field, mapper)) ->
+           ( Lwd_seq.element field.elt,
+             let value () = field.value () |> field.validate in
+             fun t -> mapper t @@ value () ))
+         ( (Lwd_seq.empty, fun _ -> Form.default),
+           fun (elts, f) (elts', f') ->
+             (Lwd_seq.concat elts elts', fun t -> f' (f t)) )
   in
-  let on_submit e =
-    on_submit @@ value ();
-    Ev.prevent_default e
+  let handler =
+    Lwd.map fields ~f:(fun (_, value) ->
+        let on_submit e =
+          on_submit @@ value Form.default;
+          Ev.prevent_default e
+        in
+        Elwd.handler Ev.submit on_submit)
   in
-  let on_submit = `P (Elwd.handler Ev.submit on_submit) in
-  let elts = List.map elts ~f:(fun e -> `P e) in
-  let ev = Option.map_or ~default:[on_submit] (List.cons on_submit) ev in
-  Elwd.form ?d ?at ~ev elts
+  let on_submit = `R handler in
+  let elts = Lwd.map fields ~f:(fun (elts, _) -> elts) in
+  let ev = Option.map_or ~default:[ on_submit ] (List.cons on_submit) ev in
+  Elwd.form ?d ?at ~ev [ `S elts ]
