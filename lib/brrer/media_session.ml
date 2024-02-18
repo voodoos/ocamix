330bb0923105362c856577bd95bdf92c90405029
Ulysse Gérard <thevoodoos@gmail.com>
2024-02-18 15:23:02 +0100

    brrer: add media_sassion.media_metadata

--- lib/brrer/media_session.ml
+++ lib/brrer/media_session.ml
@@ -6,6 +6,64 @@ external of_jv : Jv.t -> t = "%identity"
 
 let of_navigator n = Jv.get (Navigator.to_jv n) "mediaSession"
 
+module Media_metadata = struct
+  type img = { src : string; sizes : string; type' : string }
+
+  let img_to_jv img =
+    Jv.obj
+      [|
+        ("src", Jv.of_string img.src);
+        ("sizes", Jv.of_string img.sizes);
+        ("type", Jv.of_string img.type');
+      |]
+
+  let maybe_string jv prop =
+    match Jv.find jv prop with None -> "unknown" | Some jv -> Jv.to_string jv
+
+  let img_of_jv jv =
+    let src = maybe_string jv "src" in
+    let sizes = maybe_string jv "sizes" in
+    let type' = maybe_string jv "type" in
+    { src; sizes; type' }
+
+  type t = {
+    title : string;
+    artist : string;
+    album : string;
+    artwork : img list;
+  }
+
+  let to_jv t =
+    let artwork = Jv.of_list img_to_jv t.artwork in
+    Jv.obj
+      [|
+        ("title", Jv.of_string t.title);
+        ("artist", Jv.of_string t.artist);
+        ("album", Jv.of_string t.album);
+        ("artwork", artwork);
+      |]
+
+  let of_jv jv =
+    let title = maybe_string jv "title" in
+    let artist = maybe_string jv "artist" in
+    let album = maybe_string jv "album" in
+    let artwork =
+      match Jv.find jv "artwork" with
+      | None -> []
+      | Some jv -> Jv.to_list img_of_jv jv
+    in
+    { title; artist; album; artwork }
+end
+
+let metadata t = Jv.get t "metadata" |> Jv.to_option Media_metadata.of_jv
+
+let set_metadata t v =
+  Console.log [ "Set metadata"; Media_metadata.to_jv v ];
+  let metadata =
+    Jv.new' (Jv.get Jv.global "MediaMetadata") [| Media_metadata.to_jv v |]
+  in
+  Jv.set t "metadata" metadata
+
 module Action = struct
   type t = Jstr.t
 
--- lib/brrer/media_session.mli
+++ lib/brrer/media_session.mli
@@ -5,6 +5,20 @@ type t
 val of_jv : Jv.t -> t
 val of_navigator : Navigator.t -> t
 
+module Media_metadata : sig
+  type img = { src : string; sizes : string; type' : string }
+
+  type t = {
+    title : string;
+    artist : string;
+    album : string;
+    artwork : img list;
+  }
+end
+
+val metadata : t -> Media_metadata.t option
+val set_metadata : t -> Media_metadata.t -> unit
+
 module Action : sig
   type t
 
