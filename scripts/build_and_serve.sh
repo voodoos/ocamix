6eab746847a75660ce2c6ffe71a55509a4f9aa1c
Ulysse Gérard <thevoodoos@gmail.com>
2024-08-19 14:33:06 +0200

    Disable nodejs IO_URING arch fix

--- scripts/build_and_serve.sh
+++ scripts/build_and_serve.sh
@@ -1,6 +1,6 @@
 # This is required to workaround a node issue
 # see https://github.com/nodejs/node/issues/48444
-export UV_USE_IO_URING=0
+#export UV_USE_IO_URING=0
 
 opam exec -- dune build @default --profile=release --watch --terminal-persistence=preserve &
 DUNE_PID=$!
