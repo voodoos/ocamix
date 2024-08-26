d308435cbc50c763dc7f2639480e56a70b485afe
Ulysse Gérard <thevoodoos@gmail.com>
2024-08-26 22:11:29 +0200

    wip: New logger

---
+++ vendor/brr_lwd_ui/lib/common/logger.ml
@@ -0,0 +1,32 @@
+open Import
+
+open Brr
+
+type section = ..
+type active = All | Only of section list
+type levels = Log | Error | Info | Debug
+
+let logger = function
+  | Log -> Console.log
+  | Error -> Console.error
+  | Info -> Console.info
+  | Debug -> Console.debug
+
+type t = {
+  log : Console.log;
+  error : Console.log;
+  info : Console.log;
+  debug : Console.log;
+}
+
+let for_section s =
+  let log (l : Console.log) ?(fmt = "") msg =
+    let fmt = Printf.sprintf "[%s] %s" s fmt in
+    l (fmt :: msg)
+  in
+  {
+    log = log Console.log;
+    error = log Console.error;
+    info = log Console.info;
+    debug = log Console.debug;
+  }
