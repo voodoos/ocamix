66b47af53672d76ee6ab3a3e6718e8758469e678
Ulysse Gérard <thevoodoos@gmail.com>
2024-08-26 22:08:48 +0200

    virtual table: more evolved example

--- vendor/brr_lwd_ui/examples/virtual-table/main.ml
+++ vendor/brr_lwd_ui/examples/virtual-table/main.ml
@@ -1,7 +1,27 @@
 open Brr
 open Brr_lwd
+open Brr_lwd_ui
+open Brr_lwd_ui.Table
 
-let app = Elwd.div [ `P (El.txt' "toto") ]
+let data =
+  {
+    Virtual.total_items = Lwd.pure (*93_300*) 200;
+    fetch =
+      Lwd.pure (fun i ->
+          Console.log [ "Loading"; Jv.of_array Jv.of_int i ];
+          Fut.ok (Array.map (fun i -> Some i) i));
+    render =
+      Lwd.pure (fun i data ->
+          [ `P (El.txt' (string_of_int i)); `P (El.txt' (string_of_int data)) ]);
+  }
+
+let app =
+  let table = { columns = [| Columns.v "a" "5em" [ `P (El.txt' "toto") ] |] } in
+  let table = { table; row_height = Em 5. } in
+  let table = Virtual.make ~ui_table:table data in
+  Elwd.div
+    ~at:Attrs.O.(v (`P (A (At.style (Jstr.v "height:15em")))))
+    [ `R table ]
 
 let _ =
   let on_load _ =
