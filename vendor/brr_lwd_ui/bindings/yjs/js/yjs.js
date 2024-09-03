bd62e2697cceb9651439188713d986f19844018c
Ulysse Gérard <thevoodoos@gmail.com>
2024-09-03 20:50:22 +0200

    yjs: bind y-indexeddb

--- vendor/brr_lwd_ui/bindings/yjs/js/yjs.js
+++ vendor/brr_lwd_ui/bindings/yjs/js/yjs.js
@@ -1,5 +1,7 @@
 import * as Y from 'yjs'
 import { WebrtcProvider } from 'y-webrtc'
+import { IndexeddbPersistence } from 'y-indexeddb'
 
 window.yjs = Y
 window.WebrtcProvider = WebrtcProvider
+window.IndexeddbPersistence = IndexeddbPersistence
--- vendor/brr_lwd_ui/bindings/yjs/yjs.ml
+++ vendor/brr_lwd_ui/bindings/yjs/yjs.ml
@@ -1,3 +1,4 @@
 module Webrtc_provider = Webrtc_provider
+module Indexeddb_persistence = Indexeddb_persistence
 include Doc
 include Y
