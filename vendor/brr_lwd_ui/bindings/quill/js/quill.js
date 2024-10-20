fc7a6d2bb1bbbb804b028e1fc1f4d41c6136ba70
Ulysse Gérard <thevoodoos@gmail.com>
2024-10-20 12:43:21 +0200

    Also use bundler for css

--- vendor/brr_lwd_ui/bindings/quill/js/quill.js
+++ vendor/brr_lwd_ui/bindings/quill/js/quill.js
@@ -1,3 +1,6 @@
+require('quill/dist/quill.core.css')
+require('quill/dist/quill.bubble.css')
+require('quill/dist/quill.snow.css')
 
 window.Quill = require('quill').default
 window.QuillCursors = require('quill-cursors')
