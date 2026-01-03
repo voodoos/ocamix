388278741010dd03e43d839c51e7893cf4a4232b
Ulysse Gérard <thevoodoos@gmail.com>
2026-01-03 10:45:00 +0100

    wip album fetch: way too much GC activity when scrolling fast

--- bin/fetch.ml
+++ bin/fetch.ml
@@ -28,31 +28,34 @@ let tracks ranged_view i =
       | (exception _) | None -> Fut.error (`Msg "No result")
       | Some (k, t, a) -> Fut.ok (Track (k, t, a)))
 
-let albums db (ranged_view : View.ranged) keys indexes =
-  let open View in
+let album store (ranged_view : View.ranged) keys index =
   let module A_store = Db.Stores.Albums_store in
   let view = ranged_view.view in
+  let index = index + view.start_offset in
+  try
+    let key =
+      Bigarray.Array1.get keys
+      @@ View.Order.apply ~size:view.item_count ranged_view.order index
+      |> Int32.to_int (* this is lossy we could use [Jv.of_int32] *)
+    in
+    let open Fut.Result_syntax in
+    let* album =
+      A_store.get key store |> IDB.Request.fut
+      |> Fut.map (Result.map_err (fun e -> `Jv e))
+    in
+    match album with
+    | None -> Fut.error (`Msg "No result")
+    | Some album -> Fut.ok (Album album)
+  with Invalid_argument _ -> Fut.error (`Msg "No result")
+
+let albums db (ranged_view : View.ranged) keys indexes =
+  let module A_store = Db.Stores.Albums_store in
   let store =
     IDB.Database.transaction [ (module A_store) ] ~mode:Readonly db
     |> IDB.Transaction.object_store (module A_store)
   in
-  Array.map indexes ~f:(fun index ->
-      let index = index + view.start_offset in
-      try
-        let key =
-          Bigarray.Array1.get keys
-          @@ Order.apply ~size:view.item_count ranged_view.order index
-          |> Int32.to_int (* this is lossy we could use [Jv.of_int32] *)
-        in
-        let open Fut.Result_syntax in
-        let* album =
-          A_store.get key store |> IDB.Request.fut
-          |> Fut.map (Result.map_err (fun e -> `Jv e))
-        in
-        match album with
-        | None -> Fut.error (`Msg "No result")
-        | Some album -> Fut.ok (Album album)
-      with Invalid_argument _ -> Fut.error (`Msg "No result"))
+  let f = album store ranged_view keys in
+  Array.map indexes ~f
 
 let view_indexes db (ranged_view : View.ranged) ?keys indexes =
   match ranged_view.view.request.kind with
